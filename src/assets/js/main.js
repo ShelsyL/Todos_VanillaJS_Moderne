38a64d9fd66d562eec91d55eb4204b540cd0d190
Chelsea <chelsealouis.graphiste@gmail.com>
2020-11-25 19:28:00 +0100

    Intégration du template de la TodoList

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,8 @@
-alert("Coucou");
+import todos    from './data'; // On importe les todos à partir du fichier data.js
+import TodoList from './modules/TodoList';
+
+
+new TodoList({
+  el: '#app',
+  todos
+});
